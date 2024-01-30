915977e99827efe966907eec4587a95357c29581
nedew <nedew.sn@gmail.com>
2024-01-31 07:27:15 +0900

    Update global CSS and Sidebar component

--- apps/web/src/components/Sidebar/Sidebar.tsx
+++ apps/web/src/components/Sidebar/Sidebar.tsx
@@ -3,9 +3,11 @@ import React, { FC } from 'react';
 
 const Sidebar: FC = () => {
   return (
-    <aside className={'position-sticky w-[300px]'}>
-      <div>Raicho</div>
-      <div>
+    <aside className={'position-sticky w-[300px] bg-slate-200'}>
+      <div className={'flex items-center px-4 py-4'}>
+        <h1 className={'text-2xl font-bold'}>Raicho</h1>
+      </div>
+      <div className={'px-4 py-2'}>
         <ul>
           {menues.map((menu) => (
             <li key={menu.id}>
