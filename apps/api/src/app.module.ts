99affe6f5a7ee64efa6456bc7b9c2cc46dd4639c
nedew <nedew.sn@gmail.com>
2024-04-17 23:57:27 +0900

    NestJSの依存関係に@nestjs/mapped-typesを追加

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TeamModule } from './team/team.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TeamModule],
   controllers: [AppController],
   providers: [AppService],
 })
