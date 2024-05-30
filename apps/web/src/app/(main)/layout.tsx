d0704fcdcd05f654e94c4c7ab535aea90c1c1fda
nedew <nedew.sn@gmail.com>
2024-05-31 04:07:38 +0900

    refactor: Remove Sidebar component from MainLayout

--- apps/web/src/app/(main)/layout.tsx
+++ apps/web/src/app/(main)/layout.tsx
@@ -1,21 +1,17 @@
 import { Header } from '@/components/Header/Header';
 import { MainContent } from '@/components/MainContent/MainContent';
-import { Sidebar } from '@/components/Sidebar/Sidebar';
-import React, { FC } from 'react';
+import { FC, ReactNode } from 'react';
 
 type Props = {
-  children: React.ReactNode;
+  children: ReactNode;
 };
 
 const MainLayout: FC<Props> = ({ children }) => {
   return (
     <>
-      <div className={'flex flex-grow-1'}>
-        <Sidebar />
-        <div className={'flex flex-col w-full'}>
-          <Header />
-          <MainContent>{children}</MainContent>
-        </div>
+      <div className={'flex flex-col w-full'}>
+        <Header />
+        <MainContent>{children}</MainContent>
       </div>
     </>
   );
