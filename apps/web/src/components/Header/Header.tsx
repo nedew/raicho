ff7b4908d3a30e248e6f75a8912f2e78c9cad121
nedew <nedew.sn@gmail.com>
2024-07-01 00:13:57 +0900

    refactor: Update Header component styles for improved responsiveness

--- apps/web/src/components/Header/Header.tsx
+++ apps/web/src/components/Header/Header.tsx
@@ -4,12 +4,10 @@ import React, { FC } from 'react';
 export const Header: FC = () => {
   return (
     <header className={'border-l border-slate-200'}>
-      <div className={'flex items-center pt-4 pb-2 px-content-side'}>
-        <h1 className={'text-xl font-bold text-slate-700'}>
+      <div className={'flex items-center pt-2 pb-2 px-content-side'}>
+        <h1 className={'text-xl font-bold text-slate-700 mr-4'}>
           <a href="/">Raicho</a>
         </h1>
-      </div>
-      <div className={'px-3'}>
         <HeaderMenubar />
       </div>
     </header>
--- apps/web/src/components/Header/HeaderMenubar.tsx
+++ apps/web/src/components/Header/HeaderMenubar.tsx
@@ -5,8 +5,8 @@ export const HeaderMenubar: FC = () => {
   return (
     <div className={'flex m-[-1px] gap-2'}>
       {menues.map((menu) => (
-        <div key={menu.path} className={'px-3 py-3'}>
-          <a href={menu.path} className={'text-slate-600 hover:text-slate-800'}>
+        <div key={menu.path} className={'px-3 py-2'}>
+          <a href={menu.path} className={'text-sm text-slate-600 hover:text-slate-800'}>
             {menu.title}
           </a>
         </div>
