45224e1c159f770c6ac5859da22a72d02bdbcfd7
nedew <nedew.sn@gmail.com>
2024-07-01 00:17:36 +0900

    refactor: Update HeaderMenubar component to use Next.js Link for menu items

--- apps/web/src/components/Header/HeaderMenubar.tsx
+++ apps/web/src/components/Header/HeaderMenubar.tsx
@@ -1,4 +1,5 @@
 import { menues } from '@/config/menu.config';
+import Link from 'next/link';
 import { FC } from 'react';
 
 export const HeaderMenubar: FC = () => {
@@ -6,9 +7,9 @@ export const HeaderMenubar: FC = () => {
     <div className={'flex m-[-1px] gap-2'}>
       {menues.map((menu) => (
         <div key={menu.path} className={'px-3 py-2'}>
-          <a href={menu.path} className={'text-sm text-slate-600 hover:text-slate-800'}>
+          <Link href={menu.path} className={'text-sm text-slate-600 hover:text-slate-800'}>
             {menu.title}
-          </a>
+          </Link>
         </div>
       ))}
     </div>
